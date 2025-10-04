44fdf1e0db00520499d03828ab579efe89fc94a8
savan <sawanpatel2508@gmail.com>
2025-10-04 00:50:18 -0400

    Migrate `next.config.js` to ECMAScript module, integrate custom video output configuration in `vercel.json`.

--- next-frontend/next.config.mjs
+++ next-frontend/next.config.mjs
@@ -1,4 +1,4 @@
-const { withNextVideo } = require("next-video/process");
+import { withNextVideo } from "next-video/process";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -14,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextVideo(nextConfig);
+export default withNextVideo(nextConfig);
