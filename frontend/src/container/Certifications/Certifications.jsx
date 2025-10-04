2f2497df20b5d45de34160b7cb911d9028d795b6
savan <sawanpatel2508@gmail.com>
2025-10-04 13:47:05 -0400

    Remove Turbo pack from a build script, update post build to sync videos, and clean unused Swiper CSS imports

--- frontend/src/container/Certifications/Certifications.jsx
+++ frontend/src/container/Certifications/Certifications.jsx
@@ -1,8 +1,5 @@
 import { useContext, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 import "./certifications.scss";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import { CustomModal } from "../../Components/Modal/CustomModal.jsx";
